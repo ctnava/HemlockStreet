5396f478f94529c7246ffae6ae5bf6ac12287756
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-05 12:48:01 -0500

    ship

--- utils/ship.js
+++ utils/ship.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { type } = require('os');
+const { exec, execSync } = require('child_process');
 
 const ignored = [
     ".DS_Store", "node_modules", "package-lock.json", "npm-debug.log",
@@ -7,10 +7,13 @@ const ignored = [
     "downloads", "uploads", ".env", "api.code-workspace"
 ];
 
-(() => {
+function stageApi() {
+    console.log("\nPreparing API for Heroku Deployment...\n");
+
+
     const sink = "./herokuApi";
-    const sinkExists = fs.existsSync(sink);
-    if (sinkExists) {
+    if (fs.existsSync(sink)) {
+        console.log("Previous build detected! Wiping...");
         const allFiles = fs.readdirSync(sink);
         allFiles.forEach(file => { 
             const pathTo = sink + "/" + file;
@@ -18,13 +21,17 @@ const ignored = [
             if (isDir) fs.rmSync(pathTo, {recursive:true});
             else fs.rmSync(pathTo);
         });
-    } 
+    } else fs.mkdirSync(sink);
 
 
     const source = "./src/api";
+    console.log("Scanning API root...");
     const allFiles = fs.readdirSync(source);
+    console.log(`${allFiles.length} paths detected.`);
+    var accepted = 0;
     allFiles.forEach(file => {
         if (!ignored.includes(file)) {
+            accepted += 1;
             const pathTo = source + "/" + file;
             const end = sink + "/" + file;
             const isDir = (fs.lstatSync(pathTo)).isDirectory();
@@ -32,14 +39,17 @@ const ignored = [
             else fs.cpSync(pathTo, end, {recursive:true});
         }
     });
-    
+    console.log(`${accepted} paths copied!`);
+
 
     const dev1337 = sink + "/data/1337"
     const dev31337 = sink + "/data/31337"
+    console.log("Pruning devnet caches...");
     if (fs.existsSync(dev1337)) fs.rmSync(dev1337, {recursive:true});
     if (fs.existsSync(dev31337)) fs.rmSync(dev31337, {recursive:true});
 
 
+    console.log("Ensuring linux64 compatibility...");
     const pkg = JSON.parse(fs.readFileSync(sink + "/package.json"));
     const allPackages = Object.keys(pkg.dependencies);
     allPackages.forEach(name => {
@@ -49,6 +59,18 @@ const ignored = [
             delete pkg.dependencies[name];
         }
     });
-    console.log(pkg);
-    fs.writeFileSync(sink + "/package.json", JSON.stringify(pkg, null, 2));
-})();
+    fs.writeFileSync(sink + "/package.json", JSON.stringify(pkg, null, 2));    
+    console.log("\nReady to deploy!");
+}
+
+
+function pushApi() {
+    console.log("Deploying API to Heroku...\n\nAdding to git...");
+    // const output = execSync('ls', { encoding: 'utf-8' });
+    console.log("Committing changes...");
+    console.log("Pushing changes...");
+    console.log("\nComplete!\n");
+}
+
+stageApi();
+pushApi();
