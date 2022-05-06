9221a523ee8ef3dc6c83bba699049993518289ac
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-06 14:17:46 -0500

    testing

--- utils/sync.js
+++ utils/sync.js
@@ -9,21 +9,20 @@ async function synchronizeAll() {
         throw "missing branch or commitMessage";
     if (process.argv[4] !== undefined) 
         throw "did you forget to use quotation marks on your message?";
-    await git.push("root", branch, commitMessage);
-
+    
     const rootIsClean = await git.status();
     console.log("rootIsClean:", rootIsClean);
-    var repoStates = [rootIsClean]
+    if (!rootIsClean) await git.push("root", branch, commitMessage);
 
-    // var subRepos = fs.readdirSync("./src");
-    // subRepos = subRepos.filter(repo => !repo.includes("."));
-    // for await (const repo of subRepos){
-    //     const pathTo = `src/${repo}`;
-    //     const isClean = await git.status(pathTo);
-    //     console.log(`${repo}IsClean:`, isClean);
-    //     repoStates.push(isClean);
-    //     await git.push(pathTo, branch, commitMessage);
-    // }
+    var subRepos = fs.readdirSync("./src");
+    subRepos = subRepos.filter(repo => !repo.includes("."));
+    for await (const repo of subRepos){
+        const pathTo = `src/${repo}`;
+        const isClean = await git.status(pathTo);
+        console.log(`${repo}IsClean:`, isClean);
+        repoStates.push(isClean);
+        if (!isClean) await git.push(pathTo, branch, commitMessage);
+    }
 }
 
 
