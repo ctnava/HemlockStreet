3229f3a39db081dbb5193b4147d5cb30b9442617
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-07 17:57:35 -0500

    run

--- src/client/src/index.jsx
+++ src/client/src/index.jsx
@@ -6,11 +6,20 @@ import aesthetics from "./lib/aesthetics";
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App aesthetics={aesthetics} />);
+
+function Index() {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<App aesthetics={aesthetics} />);
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+}
+
+
+Index();
+
+
+export default Index; 
