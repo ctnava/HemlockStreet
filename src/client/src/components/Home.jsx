ce82a917f557f4f5c97d40f4e259761b2e77ac5f
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-06 23:02:25 -0500

    readme

--- src/client/src/components/Home.jsx
+++ src/client/src/components/Home.jsx
@@ -4,7 +4,19 @@ import React from 'react' //, useEffect, useRef
 
 
 function Home(props) {
-	return(<div></div>);
+	return(<div>
+		<h1>WELCOME</h1>
+		<p>
+		If you can read this, you either have metamask or you don't. <br/>
+		That's fine, but if you actually want to use the DeadDrop app (aka "dstor") <br/>
+		you must install metamask. Other than the app, there is literally nothing here. <br/>
+		If you are, in fact, interested in testing this app, make sure you set your network <br/>
+		to MUMBAI. If you are NOT on Mumbai, the app will perpetually search for a <br/>
+		nonexistent contract and never load because I'm too busy working on the documentation.
+		</p>
+		<a href="https://medium.com/stakingbits/how-to-connect-polygon-mumbai-testnet-to-metamask-fc3487a3871f">How to add Mumbai to your wallet</a><br/>
+		<a href="https://faucet.polygon.technology/">How to get MATIC for transactions</a>
+	</div>);
 }
 
 export default Home;
