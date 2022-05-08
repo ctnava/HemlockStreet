abe44f00c88ce230238f3fdc0535e5b6b77e344b
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-07 20:57:38 -0500

    autoinstaller

---
+++ utils/install.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const {powershell} = require('./shell.js');
+
+(async () => {
+    if (!fs.existsSync(`./node_modules`)) {
+        console.log(`Running Install on root`);
+        await powershell('install', {args:["root"]});
+    }
+
+    var subRepos = fs.readdirSync("./src");
+    subRepos = subRepos.filter(repo => !repo.includes("."));
+    for await (const repo of subRepos){
+        const pathTo = `src/${repo}`;
+        if (!fs.existsSync(`${pathTo}/node_modules`)) {
+            console.log(`Running Install on ${repo}`);
+            const opts = {args:[pathTo], v:true};
+            await powershell('install', opts);
+        }
+    }
+})();
