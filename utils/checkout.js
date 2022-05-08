82d6e35da7bddbe86b9c9024ca1baae50974f492
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-08 18:01:10 -0500

    checkout

--- utils/checkout.js
+++ utils/checkout.js
@@ -3,10 +3,14 @@ const fs = require('fs');
 
 (async () => {
     const branch = process.argv[2];
+    console.log("\nroot");
     await git.checkout("root", branch);
 
     const subRepos = (fs.readdirSync("./src")).filter(repo => !repo.includes("."));
-    for await (const repo of subRepos) {await git.checkout(`src/${repo}`, branch)}
+    for await (const repo of subRepos) {
+        console.log(`${repo}`);
+        await git.checkout(`src/${repo}`, branch);
+    }
 
     return;
 })();
